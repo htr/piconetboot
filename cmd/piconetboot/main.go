fd1ffe6f425e50d464e2fc34d863bce883c35ace
Hugo Tavares Reis <hugo.t.r@gmail.com>
2017-03-06 21:41:09 -0500

    Updated cmd/piconetboot/main.go

--- cmd/piconetboot/main.go
+++ cmd/piconetboot/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 var (
 	addr         string
@@ -9,5 +12,12 @@ var (
 )
 
 func main() {
+	flag.StringVar(&addr, "addr", "127.0.0.1:8085", "ipxe reachable local address")
+	flag.StringVar(&dataDir, "data-dir", "", "directory containing boot client definitions")
+	flag.BoolVar(&debugEnabled, "debug", false, "increases logging verbosity level")
+	flag.Parse()
+
+	// handle the debug thingy
+
 	fmt.Println("vim-go")
 }
