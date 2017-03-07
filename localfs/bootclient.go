274a2c1a25def9b07a1dd5f748803ab3c9995909
Hugo Tavares Reis <hugo.t.r@gmail.com>
2017-03-06 22:22:26 -0500

    Updated localfs/bootclient.go

--- localfs/bootclient.go
+++ localfs/bootclient.go
@@ -4,10 +4,11 @@ type bootClient struct {
 	data struct {
 		Matchers []struct {
 			Serial string
-			Asset string
-			UUID string
-			Mac string
+			Asset  string
+			UUID   string
+			Mac    string
+		}
+		BootScript string
+		Name       string
 	}
-	BootScript string
-	Name string
 }
