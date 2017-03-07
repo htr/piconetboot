33813b19bf71cad8270b51962b18bef37400e200
Hugo Tavares Reis <hugo.t.r@gmail.com>
2017-03-06 22:21:10 -0500

    Updated localfs/store.go

--- localfs/store.go
+++ localfs/store.go
@@ -1,9 +1,17 @@
 package localfs
 
-import "github.com/htr/piconetboot"
+import (
+	"net/url"
+
+	"github.com/htr/piconetboot"
+)
 
 type localFsStore struct {
 	path string
 }
 
 var _ piconetboot.BootClientStore = (*localFsStore)(nil)
+
+func (s *localFsStore) Find(filter url.Values) (piconetboot.BootClient, error) {
+	return nil, nil
+}
